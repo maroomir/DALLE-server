beaf63540bb4531024d6e1403a6295fca1400eaf
maroomir <maroomir@gmail.com>
2022-07-15 22:54:09 +0900

    Merge the MetaArtServer source-code

---
+++ metaartserver/__init__.py
@@ -0,0 +1,68 @@
+import os
+import time
+from threading import Thread
+
+from flask import Flask, jsonify, request
+from queue import Queue, Empty
+
+from metaartserver.dalle import generate
+
+REQUEST_BATCH_SIZE = 4
+PROCESS_IMAGE_CNT = 10
+CHECK_INTERVAL = 0.1
+
+REQUEST_ERR_REQUESTS_OVER = {'Error': 'Too Many Requests. Please try again layer'}
+REQUEST_ERR_IMAGES_OVER = {'Error': f'Too Many Images requested. Request no more than {PROCESS_IMAGE_CNT}'}
+REQUEST_ERR_INVALID = {'Error': 'Invalid Request'}
+REQUEST_ERR_GENERATE = {'Error': 'Image Generate Error'}
+
+
+def work_requests_handle(queue, model_path):
+    while True:
+        batch = []
+        while not (len(batch) >= REQUEST_BATCH_SIZE):
+            try:
+                batch.append(queue.get(timeout=CHECK_INTERVAL))
+            except Empty:
+                continue
+            for req in batch:
+                try:
+                    text, num_images = req['input'][0], req['input'][1]
+                    req['output'] = generate(text, num_images, model_path=model_path)
+                except:
+                    req['output'] = jsonify(REQUEST_ERR_GENERATE), 500
+
+
+def create_app():
+    app = Flask(__name__)
+    queue = Queue()
+    model_path = './16L_64HD_8H_512I_128T_cc12m_cc3m_3E.pt'
+    handler = Thread(target=work_requests_handle, args=(queue, model_path))
+    handler.start()
+
+    @app.route('/generate', methods=['POST'])
+    def generate():
+        if queue.qsize() > REQUEST_BATCH_SIZE:
+            return jsonify(REQUEST_ERR_REQUESTS_OVER), 429
+        try:
+            inputs = []
+            json_data = request.get_json()
+            text, num_images = json_data['text'], json_data['num_images']
+            if num_images > 10:
+                return jsonify(REQUEST_ERR_IMAGES_OVER), 500
+            inputs.append(text)
+            inputs.append(num_images)
+        except Exception:
+            return jsonify(REQUEST_ERR_INVALID), 500
+
+        req = {'input': inputs}
+        queue.put(req)
+        while 'output' not in req:
+            time.sleep(CHECK_INTERVAL)
+        return jsonify(req['output'])
+
+    @app.route('/health', methods=['GET'])
+    def health_check():
+        return "Health", 200
+
+    return app
---
+++ metaartserver/dalle.py
@@ -0,0 +1,61 @@
+import base64
+
+import torch
+import numpy
+
+from dalle_pytorch.tokenizer import tokenizer
+from dalle_pytorch import DALLE, VQGanVAE
+from einops import repeat
+from tqdm import tqdm
+from PIL import Image
+from io import BytesIO
+
+
+def load_model(path: str):
+    model_data = torch.load(path)
+    # clean-up the VAE model for using VQGan at this model
+    dalle_params, _, weights = model_data.pop('hparams'), model_data.pop('vae_params'), model_data.pop('weights')
+    dalle_params.pop('vae', None)
+    # load the VAE
+    vae = VQGanVAE(None, None)
+    # load the DALLE
+    model = DALLE(vae=vae, **dalle_params).cuda()
+    model.load_state_dict(weights)
+    return model
+
+
+def generate(text,
+             num_images,
+             model_path: str,
+             batch_size=4,
+             thresh=0.9):
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    try:
+        # load the DALLE model
+        dalle = load_model(model_path)
+        # tokenize the input texts
+        text_token = tokenizer.tokenize([text], dalle.text_seq_len).to(device)
+        text_token = repeat(text_token, '() n -> b n', b=num_images)
+        res, outputs = [], []
+        for x in tqdm(text_token.split(batch_size), desc=f'generating images for - {text_token}'):
+            # generate the image with input tokens
+            y = dalle.generate_images(x, filter_thres=thresh)
+            outputs.append(y)
+        outputs = torch.cat(outputs)
+        for i, y in tqdm(enumerate(outputs), desc='saving images'):
+            # convert the output to the image with 8bit scales
+            y = numpy.moveaxis(y.detach.cpu().numpy(), 0, -1)
+            y = (y * 255).astype('uint8')
+            image = Image.fromarray(y)
+            # encode the image to the string for the request POST
+            buffered = BytesIO()
+            image.save(buffered, format='JPEG')
+            img_encode = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            res.append(img_encode)
+        return res
+    except Exception as ex:
+        print('Error occur in script generating!', ex)
+        return ex
